ef7e0019b5985a3e85b7a4a9be32dc819a4b526e
Jerome Tolentino <tolent.jay@gmail.com>
2021-05-17 16:21:38 +0900

    Made window that looked like a terminal

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -2,8 +2,30 @@ import React from 'react';
 
 function Editor({ editorValueChange, input }) {
   return (
-    <div className='EditorContainer'>
-      <textarea className='Editor' value={input} onChange={editorValueChange} />
+    <div className='window'>
+      <div className='top-bar'>
+        <div className='window-action'>
+          <ul>
+            <li>
+              <a href=''></a>
+            </li>
+            <li>
+              <a href=''></a>
+            </li>
+            <li>
+              <a href=''></a>
+            </li>
+          </ul>
+        </div>
+        <div className='window-right'>EDITOR</div>
+      </div>
+      <div className='screen'>
+        <textarea
+          className='Editor'
+          value={input}
+          onChange={editorValueChange}
+        />
+      </div>
     </div>
   );
 }
